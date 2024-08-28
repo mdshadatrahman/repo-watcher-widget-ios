f57fdc4fb29aba21c1d178b32dd086f48bce696f
Shadat Rahman <shadat.rahman.464@gmail.com>
2024-08-28 23:40:49 +0600

    Initial Commit

---
+++ RepoWatcher/RepoWatcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  RepoWatcherApp.swift
+//  RepoWatcher
+//
+//  Created by Shadat Rahman on 28/8/24.
+//
+
+import SwiftUI
+
+@main
+struct RepoWatcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
